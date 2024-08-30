aff1846cdf42a4fd27462077bb166e71b04466f1
Milica <milicasalajic13@gmail.com>
2024-08-30 15:25:27 +0200

    added CategoryController, CategoryService, CategoryRepository, CategoryDTO

--- tourist_agency_project/src/main/java/com/example/demo/DTO/CategoryDTO.java
+++ tourist_agency_project/src/main/java/com/example/demo/DTO/CategoryDTO.java
@@ -1,14 +1,29 @@
 package com.example.demo.DTO;
 
+import com.example.demo.model.Category;
+import com.example.demo.model.TouristPackage;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDTO {
+
     private String categoryName;
+
+    private CategoryDTO parentCategory;
+
+    private Set<CategoryDTO> subcategories;
+
+    private Set<AllPackagesDTO> touristPackages;
+
 }
---
+++ tourist_agency_project/src/main/java/com/example/demo/controller/CategoryController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categroyService;
+}
